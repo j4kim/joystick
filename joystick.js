36939ae7a57ae5e3f55342f6c52b1f087d58dfe8
Joaquim Perez <jivkim@gmail.com>
2024-10-13 20:29:28 +0200

    remove 2 bytes that are always 127

--- joystick.js
+++ joystick.js
@@ -34,24 +34,24 @@ function keyTap(key) {
 }
 
 const bindings = {
-    "127,127,127,127,47,0": "a",
-    "127,127,127,127,79,0": "b",
-    "127,0,127,127,15,0": "up",
-    "127,255,127,127,15,0": "down",
-    "0,127,127,127,15,0": "left",
-    "255,127,127,127,15,0": "right",
-    "127,127,127,127,15,1": "tab",
-    "127,127,127,127,15,2": "x",
-    "127,127,127,127,15,4": "y",
-    "127,127,127,127,143,0": "enter",
-    "127,127,127,127,15,32": "m",
+    "127,127,47,0": "a",
+    "127,127,79,0": "b",
+    "127,0,15,0": "up",
+    "127,255,15,0": "down",
+    "0,127,15,0": "left",
+    "255,127,15,0": "right",
+    "127,127,15,1": "tab",
+    "127,127,15,2": "x",
+    "127,127,15,4": "y",
+    "127,127,143,0": "enter",
+    "127,127,15,32": "m",
 }
 
-const defaultState = "127,127,127,127,15,0"
+const defaultState = "127,127,15,0"
 
 device.on("data", function(data) {
     let arr = Array.from(data)
-    newData = [...arr.slice(0,2), ...arr.slice(3,7)].join()
+    newData = [...arr.slice(0,2), ...arr.slice(5,7)].join()
     if (newData === state) return
     state = newData
     if (newData === defaultState) return
