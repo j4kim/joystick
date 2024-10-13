089d346d222698fa5a34cb3597a7e14c53fc09c6
Joaquim Perez <jivkim@gmail.com>
2024-10-13 23:47:02 +0200

    install node serialport library and add script to listen to arduino serial port

---
+++ serial-listener.js
@@ -0,0 +1,27 @@
+const { SerialPort } = require('serialport');
+const { ReadlineParser } = require('@serialport/parser-readline')
+
+// npx @serialport/list pour lister les ports série 
+const path = '/dev/tty.usbmodemHIDPC1';
+
+// Configurer le port série avec le taux de transmission (baudRate) correct
+const port = new SerialPort({ path, baudRate: 9600 });
+
+// Utiliser un parser readline pour lire les données ligne par ligne
+const parser = port.pipe(new ReadlineParser({ delimiter: '\n' }))
+
+
+// Ouverture du port
+port.on('open', () => {
+  console.log(`Port série ${path} ouvert avec succès`);
+});
+
+// Lecture des données du port série
+parser.on('data', data => {
+  console.log(`Données reçues : ${data}`);
+});
+
+// Gestion des erreurs
+port.on('error', (err) => {
+  console.error('Erreur sur le port série : ', err.message);
+});
