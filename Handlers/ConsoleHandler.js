75e7d2b5ebbf66bc04a17d57a28f12e3ea908626
Joaquim Perez <jivkim@gmail.com>
2024-10-16 00:30:18 +0200

    ConsoleHandler layout

--- Handlers/ConsoleHandler.js
+++ Handlers/ConsoleHandler.js
@@ -1,36 +1,39 @@
 const Handler = require("./Handler");
 
 class ConsoleHandler extends Handler {
-    index = 0
+    index = 0;
     options = [
-        "DomAtHomeHandler",
-        "VolumeHandler",
-        "NumHandler",
-    ]
+        { handler: "DomAtHomeHandler", title: "Dom At Home" },
+        { handler: "VolumeHandler", title: "Volume" },
+        { handler: "NumHandler", title: "Numbers" },
+    ];
 
     constructor() {
         super();
-        this.showMenu()
+        this.showMenu();
     }
 
     up() {
         this.index = (this.index + 1) % this.options.length;
-        this.showMenu()
+        this.showMenu();
     }
     down() {
-        this.index = (this.index - 1 + this.options.length) % this.options.length;
-        this.showMenu()
+        const len = this.options.length;
+        this.index = (this.index - 1 + len) % len;
+        this.showMenu();
     }
     switch() {
-        return this.options[this.index];
+        return this.options[this.index].handler;
     }
 
     showMenu() {
         console.clear();
+        console.log("--------------------");
         this.options.forEach((option, index) => {
             const pointer = index == this.index ? ">" : " ";
-            console.log(pointer, option);
+            console.log(pointer, option.title);
         });
+        console.log("--------------------");
     }
 }
 
