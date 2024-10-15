a86aab5d3eddf2fe101e525fe141d4845a6d5e84
Joaquim Perez <jivkim@gmail.com>
2024-10-15 22:29:12 +0200

    remove volume.js and reimplement functions in sync mode in VolumeHandler

--- VolumeHandler.js
+++ VolumeHandler.js
@@ -1,15 +1,38 @@
 const Handler = require("./Handler");
-const { increaseVolume, decreaseVolume } = require("./volume");
+const { execSync } = require("child_process");
 
 class VolumeHandler extends Handler {
+    level = 0;
+    step = 6;
+
+    constructor() {
+        super();
+        this.level = this.getLevel();
+    }
+
     up() {
-        increaseVolume();
+        this.setLevel(this.level + this.step);
     }
     down() {
-        decreaseVolume();
+        this.setLevel(this.level - this.step);
     }
     switch() {
         return "ConsoleHandler";
+    }
+
+    getLevel() {
+        const stdout = execSync(
+            'osascript -e "output volume of (get volume settings)"'
+        );
+        return parseInt(stdout.toString().trim());
+    }
+    setLevel(level) {
+        if (level < 0) level = 0;
+        if (level > 100) level = 100;
+        this.level = level;
+        return execSync(
+            `osascript -e "set volume output volume ${this.level}"`
+        );
     }
 }
 
