c1221012d8930913653a12e4f47de3d33fcc1c31
Joaquim Perez <jivkim@gmail.com>
2024-10-15 23:26:17 +0200

    a new DomAtHomeHandler that opens Chrome in fullscreent at localhost:8080

---
+++ DomAtHomeHandler.js
@@ -0,0 +1,28 @@
+const VolumeHandler = require("./VolumeHandler");
+const { execSync } = require("child_process");
+
+class DomAtHomeHandler extends VolumeHandler {
+    constructor() {
+        super();
+        this.killall();
+        this.open("http://localhost:8080");
+    }
+
+    switch() {
+        this.killall();
+        return "ConsoleHandler";
+    }
+
+    open(url) {
+        execSync(`open -a "Google Chrome" --args --kiosk "${url}"`);
+    }
+    killall() {
+        try {
+            execSync('killall "Google Chrome"');
+        } catch (e) {
+            // no Google Chrome processes to kill, do nothing
+        }
+    }
+}
+
+module.exports = DomAtHomeHandler;
