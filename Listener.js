2252b55d059ff719efa1c1d89ab6df3a2183b463
Joaquim Perez <jivkim@gmail.com>
2024-10-16 00:31:46 +0200

    never nester

--- Listener.js
+++ Listener.js
@@ -11,19 +11,18 @@ class Listener {
     }
 
     onData(key) {
-        if (typeof this.handler[key] === "function") {
-            const nextHandlerName = this.handler[key]();
-            if (typeof nextHandlerName === "string") {
-                const nextHandler = Listener.HANDLERS[nextHandlerName]
-                if (nextHandler) {
-                    this.handler = new nextHandler();
-                } else {
-                    console.error(`Handler ${nextHandlerName} not registered`)
-                }
-            }
-        } else {
-            console.error(`No method ${key} in ${this.handler.constructor.name}`);
+        if (typeof this.handler[key] !== "function") {
+            return console.error(`No method ${key} in ${this.handler.constructor.name}`);
         }
+        const nextHandlerName = this.handler[key]();
+        if (typeof nextHandlerName !== "string") {
+            return;
+        }
+        const nextHandler = Listener.HANDLERS[nextHandlerName]
+        if (!nextHandler) {
+            return console.error(`Handler ${nextHandlerName} not registered`)
+        }
+        this.handler = new nextHandler();
     }
 }
 
