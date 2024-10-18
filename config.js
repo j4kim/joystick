30d51976f26d4394722efbebe0396e2b3df6d4f5
Joaquim Perez <jivkim@gmail.com>
2024-10-19 00:38:52 +0200

    add games

--- config.js
+++ config.js
@@ -24,6 +24,49 @@ const options = [
         title: "2048",
         args: { core: "2048_libretro.dylib" },
     },
+    {
+        handler: "RetroArchHandler",
+        title: "Anarch (Doom)",
+        args: { core: "anarch_libretro.dylib" },
+    },
+    {
+        handler: "RetroArchHandler",
+        title: "Gong",
+        args: { core: "gong_libretro.dylib" },
+    },
+    {
+        handler: "RetroArchHandler",
+        title: "Cannonball (OutRun)",
+        args: {
+            core: "cannonball_libretro.dylib",
+            rom: "CannonBall/CannonBall.game",
+        },
+    },
+    {
+        handler: "RetroArchHandler",
+        title: "Cave Story",
+        args: {
+            core: "nxengine_libretro.dylib",
+            rom: "/Cave Story (en)/Doukutsu.exe",
+        },
+    },
+    {
+        handler: "RetroArchHandler",
+        title: "Doom",
+        args: {
+            core: "prboom_libretro.dylib",
+            rom: "Doom/doom1.wad",
+        },
+    },
+    {
+        handler: "RetroArchHandler",
+        title: "Space Invaders",
+        args: {
+            core: "stella_libretro.dylib",
+            rom: "Atari 2600/Space Invaders.a26",
+        },
+    },
+    
 ];
 
 const retroarch = {
