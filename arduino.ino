1419aa64e07c60613506c13f687cc71983233f1a
Joaquim Perez <jivkim@gmail.com>
2024-10-18 21:13:12 +0200

    arduino code: handle more click events: press, release, dblclick, click, longclick, longpress

--- arduino.ino
+++ arduino.ino
@@ -7,6 +7,9 @@ int _CLK = 1;
 int _DT = 1;
 int _SW = 1;
 
+unsigned long pressed_at = 0;
+unsigned long released_at = 0;
+
 void setup() {
   pinMode(pinCLK, INPUT);
   pinMode(pinDT, INPUT);
@@ -23,9 +26,26 @@ void loop() {
   if (CLK != _CLK && CLK == LOW) {
     Serial.println(DT != CLK ? "up" : "down");
   }
+  
+  unsigned long t = millis();
 
-  if (SW != _SW && SW == LOW) {
-    Serial.println("switch");
+  if (_SW != SW) {
+    if (SW == LOW) {
+      Serial.println("press");
+      pressed_at = t;
+    } else {
+      Serial.println("release");
+      if (t - released_at < 300) {
+        Serial.println("dblclick");
+      } else if (t - pressed_at < 300) {
+        Serial.println("click");
+      } else {
+        Serial.println("longclick");
+      }
+      released_at = t;
+    }
+  } else if (released_at < pressed_at && t - pressed_at == 600) {
+    Serial.println("longpress");
   }
 
   _CLK = CLK;
